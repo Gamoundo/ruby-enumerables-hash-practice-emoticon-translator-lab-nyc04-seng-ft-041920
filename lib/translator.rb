7109f083f022ff5a79b4db67baaf1a65fbd063e8
Akil Griffiths <akilgg@gmail.com>
2020-04-28 18:40:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ emotions = YAML.load_file('emotions.yml')
 def load_library
   # code goes here
   loaded = {}
-  emotions.each 
+  emotions.each do |key, value|
+    emotions[key][0] = 
 end
 
 def get_japanese_emoticon
