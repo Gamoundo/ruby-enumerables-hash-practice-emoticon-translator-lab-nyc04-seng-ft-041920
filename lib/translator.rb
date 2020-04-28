391bac0924f857538084caf4fbe4ecaf1c4d9dd9
Akil Griffiths <akilgg@gmail.com>
2020-04-28 19:28:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(library, emoticon)
     if emotions[key][:english] == emoticon
       return emotions[key][:japanese]
   end
-  return "Sorry, that emoticon was not found
+  return "Sorry, that emoticon was not found"
   end
 end
 
